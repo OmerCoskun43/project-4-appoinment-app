4c0d8cd038fe5599568535441800846b4b35dfa5
AdamSmith <omercoskun4369@gmail.com>
2023-12-10 09:55:03 +0300

    finito

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,15 @@ const Home = () => {
     <div className="home_div">
       <img
         className="img1"
-        src="https://c8.alamy.com/comp/2HDXG05/hospital-building-icon-in-flat-style-medical-clinic-vector-illustration-on-isolated-background-medicine-sign-business-concept-2HDXG05.jpg"
-        width="10%"
+        src="https://img.freepik.com/free-vector/emergency-ambulance-white-background_1308-95157.jpg?w=2000"
+        width="110px"
         alt="img-1"
       />
       <h3 className="home_div_h3">CSKN HOSPITAL</h3>
       <img
         className="img2"
-        src="https://c8.alamy.com/comp/2HDXG05/hospital-building-icon-in-flat-style-medical-clinic-vector-illustration-on-isolated-background-medicine-sign-business-concept-2HDXG05.jpg"
-        width="10%"
+        src="https://img.freepik.com/free-vector/emergency-ambulance-white-background_1308-95157.jpg?w=2000"
+        width="110px"
         alt="img-1"
       />
     </div>
